dd6064ffb53942758a9638f90f41ae174d89750b
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2023-01-16 18:37:19 -0500

    Fix the handling of {% raw %} blocks

--- src/jinjax/jinjax.py
+++ src/jinjax/jinjax.py
@@ -1,5 +1,6 @@
 import re
 import typing as t
+from uuid import uuid4
 
 from jinja2.ext import Extension
 from jinja2.exceptions import TemplateSyntaxError
@@ -15,17 +16,17 @@ INLINE_CALL = INLINE_CALL.replace("<CMD>", RENDER_CMD)
 ATTR_START = "{"
 ATTR_END = "}"
 
-re_raw = r"\{%-?\s*raw\s*-?%\}.+\{%-?\s*endraw\s*-?%\}"
-RX_RAW = re.compile(re_raw, re.VERBOSE | re.DOTALL)
+re_raw = r"\{%-?\s*raw\s*-?%\}.+?\{%-?\s*endraw\s*-?%\}"
+RX_RAW = re.compile(re_raw, re.DOTALL)
 
 re_tag_name = r"([0-9A-Za-z_-]+\.)*[A-Z][0-9A-Za-z_-]*"
 re_raw_attrs = r"(?P<attrs>[^\>]*)"
 re_content = r"(?P<content>.*)"
 re_tag = rf"<(?P<tag>{re_tag_name}){re_raw_attrs}(/>|>{re_content}</{re_tag_name}>)"
-RX_TAG = re.compile(re_tag, re.VERBOSE | re.DOTALL)
+RX_TAG = re.compile(re_tag, re.DOTALL)
 
 re_uncloded = rf"<(?P<tag>{re_tag_name}){re_raw_attrs}>"
-RX_UNCLOSED = re.compile(re_uncloded, re.VERBOSE | re.DOTALL)
+RX_UNCLOSED = re.compile(re_uncloded, re.DOTALL)
 
 re_attr_name = r"(?P<name>[a-zA-Z_][0-9a-zA-Z_-]*)"
 re_equal = r"\s*=\s*"
@@ -38,10 +39,6 @@ re_attr = rf"""
 """
 RX_ATTR = re.compile(re_attr, re.VERBOSE | re.DOTALL)
 
-MSG_UNCLOSED_TAG = "Unclosed component {TAG}"
-
-TRawRanges = list[tuple[int, int]]
-
 
 class JinjaX(Extension):
     def preprocess(
@@ -50,31 +47,46 @@ class JinjaX(Extension):
         name: t.Optional[str] = None,
         filename: t.Optional[str] = None,
     ) -> str:
-        raw_ranges = self._get_raw_ranges(source)
-        pos = 0
-        while True:
-            match = RX_TAG.search(source, pos=pos)
-            if not match:
-                break
-            tag_start = match.start(0)
-            for start, end in raw_ranges:
-                if start <= tag_start <= end:
-                    pos = end
-                    break
-            else:
-                source = RX_TAG.sub(self._process_tag, source)
-
-        self._check_for_unclosed(source, raw_ranges, name, filename)
+        self.__raw_blocks = {}
+        source = self._replace_raw_blocks(source)
+        source = self._process_tags(source)
+        self._check_for_unclosed(source, name, filename)
+        source = self._restore_raw_blocks(source)
+        self.__raw_blocks = {}
         return source
 
-    def _get_raw_ranges(self, source: str) -> TRawRanges:
-        return [m.span(0) for m in RX_RAW.finditer(source) if m]
+    def _replace_raw_blocks(self, source: str) -> str:
+        while True:
+            match = RX_RAW.search(source)
+            if not match:
+                break
+            source = RX_RAW.sub(self._replace_raw_block, source)
+        return source
+
+    def _replace_raw_block(self, match: re.Match) -> str:
+        uid = f"--RAW-{uuid4().hex}--"
+        self.__raw_blocks[uid] = match.group(0)
+        return uid
+
+    def _restore_raw_blocks(self, source: str) -> str:
+        for uid, code in self.__raw_blocks.items():
+            source = source.replace(uid, code)
+        return source
+
+    def _process_tags(self, source: str) -> str:
+        while True:
+            match = RX_TAG.search(source)
+            if not match:
+                break
+            source = RX_TAG.sub(self._process_tag, source)
+
+        return source
 
     def _process_tag(self, match: re.Match) -> str:
         tag = match.group("tag")
         attrs = (match.group("attrs") or "").strip()
         content = (match.group("content") or "").strip()
-        logger.debug(f"<{tag}> {attrs} {'inline' if not content else ''}")
+        logger.debug(f"{tag} {attrs} {'inline' if not content else ''}")
         attrs_list = self._parse_attrs(attrs)
         return self._build_call(tag, attrs_list, content)
 
@@ -111,31 +123,19 @@ class JinjaX(Extension):
                 .replace("<ATTRS>", str_attrs)
                 .replace("<CONTENT>", content)
             )
-
-        logger.debug(f"-> {call}")
         return call
 
     def _check_for_unclosed(
         self,
         source: str,
-        raw_ranges: TRawRanges,
         name: t.Optional[str] = None,
         filename: t.Optional[str] = None,
     ) -> None:
-        pos = 0
-        while True:
-            match = RX_UNCLOSED.search(source, pos=pos)
-            if not match:
-                break
-            tag_start = match.start(0)
-            for start, end in raw_ranges:
-                if start <= tag_start <= end:
-                    pos = end
-                    break
-            else:
-                raise TemplateSyntaxError(
-                    message=MSG_UNCLOSED_TAG.format(TAG=match.group(0)),
-                    lineno=source[:tag_start].count("\n") + 1,
-                    name=name,
-                    filename=filename
-                )
+        match = RX_UNCLOSED.search(source)
+        if match:
+            raise TemplateSyntaxError(
+                message=f"Unclosed component {match.group(0)}",
+                lineno=source[:match.start(0)].count("\n") + 1,
+                name=name,
+                filename=filename
+            )
