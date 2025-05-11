2429eaa07b971309b5e0d3b5ff4c5ee59cc1d9c7
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2025-05-10 22:40:33 -0500

    Replace regex with parser

--- src/jinjax/jinjax.py
+++ src/jinjax/jinjax.py
@@ -27,9 +27,7 @@ RX_RAW = re.compile(re_raw, re.DOTALL)
 re_tag_prefix = r"([0-9A-Za-z_-]+\:)?"
 re_tag_path = r"([0-9A-Za-z_-]+\.)*[A-Z][0-9A-Za-z_-]*"
 re_tag_name = rf"{re_tag_prefix}{re_tag_path}"
-re_raw_attrs = r"(?P<attrs>[^\>]*)"
-re_tag = rf"<(?P<tag>{re_tag_name}){re_raw_attrs}\s*/?>"
-RX_TAG = re.compile(re_tag)
+RX_TAG_NAME = re.compile(rf"<(?P<tag>{re_tag_name})(\s|\n|/|>)")
 
 re_attr_name = r""
 re_equal = r""
@@ -45,6 +43,9 @@ RX_ATTR = re.compile(re_attr, re.VERBOSE | re.DOTALL)
 
 
 class JinjaX(Extension):
+    _name: str | None = None
+    _filename: str | None = None
+
     def preprocess(
         self,
         source: str,
@@ -54,13 +55,13 @@ class JinjaX(Extension):
         self.__raw_blocks = {}
         self._name = name
         self._filename = filename
-        source = self._replace_raw_blocks(source)
-        source = self._process_tags(source)
-        source = self._restore_raw_blocks(source)
+        source = self.replace_raw_blocks(source)
+        source = self.process_tags(source)
+        source = self.restore_raw_blocks(source)
         self.__raw_blocks = {}
         return source
 
-    def _replace_raw_blocks(self, source: str) -> str:
+    def replace_raw_blocks(self, source: str) -> str:
         while True:
             match = RX_RAW.search(source)
             if not match:
@@ -76,46 +77,106 @@ class JinjaX(Extension):
         self.__raw_blocks[uid] = do_forceescape(match.group(0))
         return uid
 
-    def _restore_raw_blocks(self, source: str) -> str:
+    def restore_raw_blocks(self, source: str) -> str:
         for uid, code in self.__raw_blocks.items():
             source = source.replace(uid, code)
         return source
 
-    def _process_tags(self, source: str) -> str:
+    def process_tags(self, source: str) -> str:
         while True:
-            match = RX_TAG.search(source)
+            match = RX_TAG_NAME.search(source)
             if not match:
                 break
-            source = self._process_tag(source, match)
+            source = self.replace_tag(source, match)
         return source
 
-    def _process_tag(self, source: str, match: re.Match) -> str:
-        start, end = match.span(0)
-        tag = match.group("tag")
-        attrs = (match.group("attrs") or "").strip()
-        inline = match.group(0).endswith("/>")
+    def replace_tag(self, source: str, match: re.Match) -> str:
+        start, curr = match.span(0)
         lineno = source[:start].count("\n") + 1
 
-        logger.debug(f"{tag} {attrs} {'inline' if not inline else ''}")
+        tag = match.group("tag")
+        attrs, end = self._parse_opening_tag(source, start=curr - 1)
+        if end == -1:
+            raise TemplateSyntaxError(
+                message=f"Syntax error `{tag}`",
+                lineno=lineno,
+                name=self._name,
+                filename=self._filename
+            )
+
+        inline = source[end - 2:end] == "/>"
         if inline:
             content = ""
         else:
-            end_tag = f"</{tag}>"
-            index = source.find(end_tag, end, None)
+            close_tag = f"</{tag}>"
+            index = source.find(close_tag, end, None)
             if index == -1:
                 raise TemplateSyntaxError(
-                    message=f"Unclosed component {match.group(0)}",
+                    message=f"Unclosed component {tag}",
                     lineno=lineno,
                     name=self._name,
                     filename=self._filename
                 )
             content = source[end:index]
-            end = index + len(end_tag)
+            end = index + len(close_tag)
 
         attrs_list = self._parse_attrs(attrs)
         repl = self._build_call(tag, attrs_list, content)
 
         return f"{source[:start]}{repl}{source[end:]}"
+
+    def _parse_opening_tag(self, source: str, start: int) -> tuple[str, int]:
+        max = len(source)
+        in_single_quotes = in_double_quotes = in_braces = False   # dentro de '…'  /  "…"
+        i = start
+        end = -1
+
+        while i < max:
+            ch = source[i]
+            ch2 = source[i:i + 2]
+            # print(ch, ch2, in_single_quotes, in_double_quotes, in_braces)
+
+            # Detecta {{ … }} sólo cuando NO estamos dentro de comillas
+            if not in_single_quotes and not in_double_quotes:
+                if ch2 == "{{":
+                    if in_braces:
+                        # Unmatched braces!
+                        break
+                    in_braces = True
+                    i += 2
+                    continue
+
+                if ch2 == "}}":
+                    if not in_braces:
+                        # Unmatched braces!
+                        break
+                    in_braces = False
+                    i += 2
+                    continue
+
+            if (in_single_quotes or in_double_quotes) and ch2 in (r'\"', r"\'"):
+                i += 2
+                continue
+
+            if ch == "'" and not in_double_quotes:
+                in_single_quotes = not in_single_quotes
+                i += 1
+                continue
+
+            if ch == '"' and not in_single_quotes:
+                in_double_quotes = not in_double_quotes
+                i += 1
+                continue
+
+            # Fin de la etiqueta: ‘>’ fuera de comillas y fuera de {{ … }}
+            if ch == ">" and not (in_single_quotes or in_double_quotes or in_braces):
+                end = i + 1
+                break
+
+            i += 1
+
+        attrs = source[start:end].strip().removesuffix("/>").removesuffix(">")
+        return attrs, end
 
     def _parse_attrs(self, attrs: str) -> list[tuple[str, str]]:
         attrs = attrs.replace("\n", " ").strip()
